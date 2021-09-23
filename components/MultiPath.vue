b0983e3bff3b0da7e2af4744b161fd5e2693c0e4
Robin Watson <drumsrobin@gmail.com>
2021-09-22 21:55:41 -0400

    add correct file path for bg img

---
+++ components/MultiPath.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="multi-path tw-h-screen">
+    <div
+      v-for="(item, index) in categories"
+      :key="index"
+      class="tw-flex tw-flex-col tw-items-center tw-p-8 tw-text-center tw-h-2/6"
+      :style="`background: url(${item.backgroundImage})`"
+    >
+      <h4 class="tw-mb-4">{{ item.title }}</h4>
+      <p>{{ item.description }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    categories: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss"></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="index">
-    <h1 class="tw-font-default">Hey peeps</h1>
-    <!-- TODO: add multipath component -->
+    <MultiPath :categories="categories"></MultiPath>
   </div>
 </template>
 
 <script>
-export default {};
+import MultiPath from "@/components/MultiPath.vue";
+import MusicImg from "@/static/music.jpg";
+import VideoImg from "@/static/video.png";
+import PhotoImg from "@/static/gaze.jpg";
+export default {
+  components: {
+    MultiPath
+  },
+  data() {
+    return {
+      categories: {
+        music: {
+          title: "Listen",
+          description: "Check out my music",
+          backgroundImage: MusicImg
+        },
+        video: {
+          title: "Watch",
+          description: "Check out some of my new productions",
+          backgroundImage: VideoImg
+        },
+        photography: {
+          title: "Gaze",
+          description: "Take a look at my gallery",
+          backgroundImage: PhotoImg
+        }
+      }
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
