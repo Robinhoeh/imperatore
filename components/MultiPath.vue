87bb4390254a1422825350f728df4acf5456aa90
Robin Watson <drumsrobin@gmail.com>
2021-09-24 21:09:51 -0400

    basic responsive multipage --v1-done

--- components/MultiPath.vue
+++ components/MultiPath.vue
@@ -1,11 +1,11 @@
 <template>
   <section
-    class="multi-path tw-grid tw-grid-flow-col tw-grid-rows-3 tw-gap-0 tw-h-screen"
+    class="multi-path tw-grid tw-grid-flow-col md:tw-grid-rows-none tw-grid-rows-3 tw-gap-0 tw-h-screen"
   >
     <div
       v-for="(item, index) in categories"
       :key="index"
-      class="tw-p-8 tw-text-center "
+      class="tw-p-8 tw-text-center md:tw-flex md:tw-justify-center md:tw-items-center md:tw-flex-col md:tw-flex-1"
       :style="
         `background: url(${item.backgroundImage}) no-repeat center center`
       "
