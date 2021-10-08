ba328966d544ac604936248e807fb5dc482981e6
Robin Watson <drumsrobin@gmail.com>
2021-10-07 20:47:54 -0400

    add music links for albums

--- pages/_slug/music.vue
+++ pages/_slug/music.vue
@@ -36,18 +36,21 @@
       </div>
     </section>
     <section class="discography-section tw-mb-32">
-      <h4 class="tw-border-b tw-pb-5 tw-max-w-[420px] lg:tw-text-5xl tw-mb-8">
+      <h4 class="tw-border-b tw-pb-5 tw-max-w-[420px] lg:tw-text-5xl tw-mb-4">
         Discography
       </h4>
-      <div>
-        <h4>Remixes</h4>
+      <div class="tw-mb-14 tw-flex tw-flex-col tw-items-center">
         <div
-          class="tw-bg-contain tw-bg-center tw-h-52`"
-          :style="
-            `background: url(${discography.remix1.backgroundImg}) no-repeat center center contain`
-          "
+          class="tw-bg-center tw-mb-11 tw-flex tw-flex-col tw-items-center"
+          v-for="album in discography"
+          :key="album.id"
         >
-          {{ discography.remix1.title }}
+          <h5 class="tw-mb-2">{{ album.title }}</h5>
+          <h5 class="tw-mb-2" v-if="album.subTitle">{{ album.subTitle }}</h5>
+          <h5 class="tw-mb-2" v-if="album.album">{{ album.album }}</h5>
+          <a :href="album.spotifyLink" target="_blank">
+            <img :src="album.backgroundImg" alt="" class="tw-w-48" />
+          </a>
         </div>
       </div>
       <div>
@@ -109,27 +112,39 @@ export default {
   data() {
     return {
       themeMode: "light",
-      discography: {
-        remix1: {
+      discography: [
+        {
           title: "Dona Ontete",
           subTitle: "Imperatore Remix",
           album: "Mexe Mexe",
-          backgroundImg: Dona
+          backgroundImg: Dona,
+          spotifyLink:
+            "https://open.spotify.com/track/7ylgplJpJgpdxGrvBQQPPV?si=3cde3787593f4abf",
+          id: 1
         },
-        remix2: {
+        {
           title: "Festa da Tubarao",
           subTitle: "Remix",
-          backgroundImg: Tubarao
+          backgroundImg: Tubarao,
+          spotifyLink:
+            "https://open.spotify.com/track/1djUMs5HUoB2w3yOoBQgg9?si=761ad4ccb84f41d2",
+          id: 2
         },
-        album1: {
+        {
           title: "Boteco Eletro 1",
-          backgroundImg: Boteco1
+          backgroundImg: Boteco1,
+          spotifyLink:
+            "https://open.spotify.com/album/3WojC86QGhmaAZaCD0dLWv?si=yfiBEOR5Sj-Wmk-oE755Fw&dl_branch=1",
+          id: 3
         },
-        album2: {
+        {
           title: "Boteco Eletro vol.2",
-          backgroundImg: Boteco2
+          backgroundImg: Boteco2,
+          spotifyLink:
+            "https://open.spotify.com/album/0KGrzYGeBFb35KlL1RQXBB?si=_Q8ZpjmIRLqRoXLAQOdlPw&dl_branch=1",
+          id: 4
         }
-      },
+      ],
       biography: {
         descriptionOne:
           "Brazilian, musician, producer, photographer, and activist could describe Ricardo Imperatore.  Like the work of multi-disciplinary artists of all sorts, each of Imperatore’s art practices influences the others.  His brand of documentary photography:  informal and playful, involved and passionate - reflects his music: electric, eclectic, personal and political... uniquely entertaining.  His work is wrapped around his love and loss relationship to Rio de Janeiro."
