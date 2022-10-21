0773203339ec29495df7b9267d89105bc4c76b99
robinwatson <drumsrobin@gmail.com>
2022-10-20 22:29:22 -0400

    add img link

--- pages/photography.vue
+++ pages/photography.vue
@@ -3,13 +3,16 @@
     <h2
       class="tw-text-2xl md:tw-text-[60px] lg:tw-text-[80px] tw-font-semibold tw-mb-2 md:tw-mb-5 lg:tw-mb-8"
     ></h2>
-    <img
-      src="~/assets/images/gamboa-dream.png"
-      alt=""
-      class="tw-max-h-80 xl:tw-max-h-96"
-    />
+
     <a href="/ricardophotostory.pdf" download="">
-      <h3>Download: Ricardo Imperator</h3>
+      <img
+        src="~/assets/images/gamboa-dream.png"
+        alt=""
+        class="tw-max-h-80 xl:tw-max-h-96"
+      />
+
+      <p>Download:</p>
+      <h3>Ricardo Imperatore</h3>
       <h4>"Gamboa's Dream</h4>
     </a>
     <!-- <div>
